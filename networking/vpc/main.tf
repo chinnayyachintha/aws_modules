85b2d45a03df7aa3807dd99bc75c2104ab43e345
chinnayyachintha <chinthayadav6@gmail.com>
2025-02-21 14:50:22 +0530

    First release of networking module

---
+++ networking/vpc/main.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.availability_zone
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
