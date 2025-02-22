27f38fcf836533b90f62554a85531aafd6130235
Chinnayya Chintha <135199006+chinnayyachintha@users.noreply.github.com>
2025-02-22 09:22:36 +0530

    Update outputs.tf

--- networking/vpc/outputs.tf
+++ networking/vpc/outputs.tf
@@ -1,15 +1,34 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The ID of the created VPC"
+  value       = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public.id
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public.id
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.private.id
+  description = "The ID of the private subnet"
+  value       = aws_subnet.private.id
 }
 
 output "internet_gateway_id" {
-  value = aws_internet_gateway.main.id
+  description = "The ID of the internet gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_cidr_block" {
+  description = "The CIDR block of the public subnet"
+  value       = aws_subnet.public.cidr_block
+}
+
+output "private_subnet_cidr_block" {
+  description = "The CIDR block of the private subnet"
+  value       = aws_subnet.private.cidr_block
 }
